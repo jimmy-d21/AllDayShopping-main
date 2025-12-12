57c375d4395d3a8b4c1b5643d4b62c80a915b00e
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-12-12 17:15:23 +0800

    Create the layout and outlet for admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import Stores from "./pages/Stores";
+import Approve from "./pages/Approve";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="stores" element={<Stores />} />
+        <Route path="approve-store" element={<Approve />} />
+      </Route>
     </Routes>
   );
 };
--- admin/src/pages/Layout.jsx
+++ admin/src/pages/Layout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div>
+    <div className="min-h-screen flex gap-10">
       <h1>Layout</h1>
+      <div>
+        <Outlet />
+      </div>
     </div>
   );
 };
