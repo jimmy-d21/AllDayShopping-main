d7e7f601f4f9de208908a5609c321f601e7ef0b4
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-12-12 17:10:42 +0800

    Create all the folder for admin and create all the page for admin

---
+++ admin/src/pages/Approve.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Approve = () => {
+  return (
+    <div>
+      <h1>Approve</h1>
+    </div>
+  );
+};
+
+export default Approve;
---
+++ admin/src/pages/Stores.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Stores = () => {
+  return (
+    <div>
+      <h1>Stores</h1>
+    </div>
+  );
+};
+
+export default Stores;
