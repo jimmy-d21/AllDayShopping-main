b8aca073b25d4a808745ed5fc2c8c9a78ef3e06a
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-12-12 18:18:56 +0800

    Create a dummy all stores

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,3 +4,41 @@ export const dummyAdminDashboard = {
   totalOrders: 5,
   totalRevenue: 4998,
 };
+
+export const dummyAllStores = [
+  {
+    _id: "69399988981746daad084d84",
+    owner: "69398206c22275884547cbb6",
+    storeLogo:
+      "https://res.cloudinary.com/dytbrbgc3/image/upload/v1765382546/zkcgeaysdjod6u6t0j1w.png",
+    username: "jimmyedelacruzStore",
+    name: "jimmyedelacruzStore",
+    description: "Buy Our Products!",
+    email: "jimmyedelacruz2@gmail.com",
+    phone: "0937760355",
+    address: "Aim 3, village\nBlock 2 Lot 14",
+    requestStatus: "approve",
+    isActive: true,
+    createdAt: "2025-12-10T16:02:16.711Z",
+    updatedAt: "2025-12-10T16:04:14.719Z",
+    __v: 0,
+  },
+  {
+    _id: "69398187ff3b84436660d59d",
+    owner: "69397f9137bb24a4eda1198d",
+    storeLogo:
+      "https://res.cloudinary.com/dytbrbgc3/image/upload/v1765376401/uid7c4nlx50elhypxd3v.jpg",
+    username: "jimmyedelacruz1",
+    name: "JimmyPlantita Corner",
+    description:
+      "Indoor & outdoor plants lover’s paradise! Rare monstera, philodendron, calathea, succulents, cacti, pots, soil & fertilizers. With plant care guide · Same-day delivery within Metro Manila · Plantito/Plantita approved!",
+    email: "jimmyplantita@gmail.com",
+    phone: "+63 945 777 8899",
+    address: "23 Kamias Road, Barangay Sikatuna Village",
+    requestStatus: "approve",
+    isActive: true,
+    createdAt: "2025-12-10T14:19:51.495Z",
+    updatedAt: "2025-12-10T16:03:56.960Z",
+    __v: 0,
+  },
+];
